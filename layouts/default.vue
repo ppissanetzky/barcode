e838278b06584248b5ca2f7b585e433b827dc132
Pablo Pissanetzky <ppissanetzky@netflix.com>
2021-01-14 11:24:28 -0800

    Added a navigation drawer and got rid of the buttons at the top

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,47 @@
     <v-system-bar app />
 
     <v-app-bar app>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn text to="/">
-        DBTC
-      </v-btn>
-      <v-btn text to="/equipment">
-        Equipment
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title>BARcode</v-toolbar-title>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>BARcode</v-list-item-title>
+        </v-list-item>
+        <v-divider />
+        <v-list-item to="/">
+          <v-list-item-icon><v-icon>mdi-fishbowl-outline</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Your collection</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/dbtc">
+          <v-list-item-icon><v-icon>mdi-sitemap</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>DBTC collection</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/equipment">
+          <v-list-item-icon><v-icon>mdi-toolbox-outline</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Equipment</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+        <v-list-item href="https://bareefers.org/">
+          <v-list-item-icon><v-icon>mdi-forum-outline</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Bay Area Reefers</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <nuxt />
@@ -25,7 +57,7 @@
 export default {
   data () {
     return {
-      title: 'BARcode'
+      drawer: false
     }
   }
 }
