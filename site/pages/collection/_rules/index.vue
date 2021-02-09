6cc3e2199abcba3863007eca56e25f8243d62bb9
Pablo Pissanetzky <ppissanetzky@netflix.com>
2021-02-09 13:55:28 -0800

    Better filtering and search

--- site/pages/collection/_rules/index.vue
+++ site/pages/collection/_rules/index.vue
@@ -5,139 +5,177 @@
         <h1>{{ this.$route.params.rules.toUpperCase() }} Collection</h1>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="auto">
+        <v-dialog
+          v-model="showFilter"
+          max-width="375px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon left>
+                mdi-tune-vertical
+              </v-icon>
+              Filter
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title>Filter</v-card-title>
+            <v-card-actions>
+              <v-autocomplete
+                v-model="typeFilter"
+                label="Type"
+                :items="types"
+                clearable
+                hide-details
+                solo
+              />
+            </v-card-actions>
+            <v-card-actions>
+              <v-autocomplete
+                v-model="memberFilter"
+                label="Member"
+                :items="members"
+                item-value="ownerId"
+                item-text="name"
+                clearable
+                hide-details
+                solo
+              />
+            </v-card-actions>
+            <v-card-actions>
+              <v-checkbox
+                v-model="haveFilter"
+                label="You don't have it"
+                hide-details
+              />
+              <v-spacer />
+              <v-checkbox
+                v-model="availableFilter"
+                label="Is available"
+                hide-details
+              />
+            </v-card-actions>
+            <v-card-actions>
+              <v-btn color="secondary" @click="clearFilter">
+                Clear
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+      <v-col cols="auto">
+        <v-autocomplete
+          v-model="nameFilter"
+          placeholder="Type to search by name"
+          :items="names"
+          clearable
+          hide-details
+          outlined
+          dense
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align-self="center" cols="auto">
+        <v-chip v-if="typeFilter" label close class="my-1 mr-1" @click:close="typeFilter=''">
+          {{ typeFilter }}
+        </v-chip>
+        <v-chip v-if="memberFilter" label close class="my-1 mr-1" @click:close="memberFilter=''">
+          {{ memberFilterName }}
+        </v-chip>
+        <v-chip v-if="haveFilter" label close class="my-1 mr-1" @click:close="haveFilter=false">
+          You don't have it
+        </v-chip>
+        <v-chip v-if="availableFilter" label close class="my-1 mr-1" @click:close="availableFilter=false">
+          Is available
+        </v-chip>
+      </v-col>
+      <v-col>
+        <v-spacer />
+      </v-col>
+    </v-row>
 
     <!-- The cards of mothers -->
     <v-row>
-      <v-col>
-        <v-container fluid>
-          <v-data-iterator :items="filteredMothers">
-            <!-- Controls for filtering -->
-
-            <template v-slot:header>
-              <v-container fluid>
-                <v-row>
-                  <v-col cols="auto">
-                    <v-autocomplete
-                      v-model="typeFilter"
-                      label="Type"
-                      :items="types"
-                      clearable
-                      hide-details
-                      solo
-                    />
-                  </v-col>
-                  <v-col cols="auto">
-                    <v-autocomplete
-                      v-model="memberFilter"
-                      label="Member"
-                      :items="members"
-                      item-value="ownerId"
-                      item-text="name"
-                      clearable
-                      hide-details
-                      solo
-                    />
-                  </v-col>
-                  <v-col cols="auto">
-                    <v-autocomplete
-                      v-model="nameFilter"
-                      label="Name"
-                      :items="names"
-                      clearable
-                      hide-details
-                      solo
-                    />
-                  </v-col>
-                  <v-col cols="auto">
-                    <v-checkbox
-                      v-model="haveFilter"
-                      label="You don't have it"
-                      hide-details
-                    />
-                  </v-col>
-                  <v-col cols="auto">
-                    <v-checkbox
-                      v-model="availableFilter"
-                      label="Is available"
-                      hide-details
-                    />
-                  </v-col>
-                  <v-col>
-                    <v-spacer />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </template>
-
-            <template v-slot:default="{ items }">
-              <v-row>
-                <v-col
-                  v-for="m in items"
-                  :key="m.motherId"
-                  cols="auto"
+      <v-col
+        v-for="m in filteredMothers"
+        :key="m.motherId"
+        cols="auto"
+      >
+        <bc-editable-frag-card
+          :frag="m"
+          :user="user"
+          show-owner
+        >
+          <template v-slot:first-tabs>
+            <v-tab>
+              <v-icon>mdi-account-multiple-outline</v-icon>
+            </v-tab>
+          </template>
+          <template v-slot:first-tabs-items>
+            <v-tab-item>
+              <v-card-title>Owners and frags available</v-card-title>
+              <v-card-text v-if="!m.owners.length && m.isAlive">
+                Only <a :href="m.owner.viewUrl" target="_blank">{{ you(m.owner) }}</a>
+              </v-card-text>
+              <v-card-text v-else-if="!m.owners.length && !m.isAlive">
+                Only <a :href="m.owner.viewUrl" target="_blank">{{ you(m.owner) }}</a>, but it is dead
+              </v-card-text>
+              <v-card-text v-else>
+                <v-simple-table>
+                  <tbody>
+                    <tr
+                      v-for="owner in m.owners"
+                      :key="owner.ownerId"
+                    >
+                      <td>
+                        <a :href="owner.viewUrl" target="_blank">{{ you(owner, true) }}</a>
+                        {{ owner.location ? ' in ' + owner.location : '' }}
+                      </td>
+                      <td
+                        class="text-right"
+                      >
+                        {{ owner.fragsAvailable }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </v-simple-table>
+              </v-card-text>
+              <v-card-text v-if="m.owners.length > 1">
+                <v-btn
+                  small
+                  color="secondary"
+                  :to="`/kids/${m.motherId}`"
                 >
-                  <bc-editable-frag-card
-                    :frag="m"
-                    :user="user"
-                    show-owner
-                  >
-                    <template v-slot:first-tabs>
-                      <v-tab>
-                        <v-icon>mdi-account-multiple-outline</v-icon>
-                      </v-tab>
-                    </template>
-                    <template v-slot:first-tabs-items>
-                      <v-tab-item>
-                        <v-card-title>Owners and frags available</v-card-title>
-                        <v-card-text v-if="!m.owners.length && m.isAlive">
-                          Only <a :href="m.owner.viewUrl" target="_blank">{{ you(m.owner) }}</a>
-                        </v-card-text>
-                        <v-card-text v-else-if="!m.owners.length && !m.isAlive">
-                          Only <a :href="m.owner.viewUrl" target="_blank">{{ you(m.owner) }}</a>, but it is dead
-                        </v-card-text>
-                        <v-card-text v-else>
-                          <v-simple-table>
-                            <tbody>
-                              <tr
-                                v-for="owner in m.owners"
-                                :key="owner.ownerId"
-                              >
-                                <td>
-                                  <a :href="owner.viewUrl" target="_blank">{{ you(owner, true) }}</a>
-                                  {{ owner.location ? ' in ' + owner.location : '' }}
-                                </td>
-                                <td
-                                  class="text-right"
-                                >
-                                  {{ owner.fragsAvailable }}
-                                </td>
-                              </tr>
-                            </tbody>
-                          </v-simple-table>
-                        </v-card-text>
-                        <v-card-text v-if="m.owners.length > 1">
-                          <v-btn
-                            small
-                            color="secondary"
-                            :to="`/kids/${m.motherId}`"
-                          >
-                            See all frags
-                          </v-btn>
-                        </v-card-text>
-                      </v-tab-item>
-                    </template>
-                  </bc-editable-frag-card>
-                </v-col>
-              </v-row>
-            </template>
-          </v-data-iterator>
-        </v-container>
+                  See all frags
+                </v-btn>
+              </v-card-text>
+            </v-tab-item>
+          </template>
+        </bc-editable-frag-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
 import BcEditableFragCard from '~/components/BcEditableFragCard.vue'
+const compare = (a, b) => {
+  const la = a.toLowerCase()
+  const lb = b.toLowerCase()
+  if (la < lb) {
+    return -1
+  }
+  if (la > lb) {
+    return 1
+  }
+  return 0
+}
 export default {
   components: { BcEditableFragCard },
   async fetch () {
@@ -151,17 +189,10 @@ export default {
     const members = []
     mothers.forEach(({ owners, name, type }) => {
       owners.forEach(owner => members.push(owner))
-      this.names.push(name)
       this.types.push(type)
     })
-    members.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1
-      } else if (a.name > b.name) {
-        return 1
-      }
-      return 0
-    })
+    members.sort((a, b) => compare(a.name, b.name))
+    this.types.sort(compare)
     this.members = members
   },
   data () {
@@ -170,14 +201,32 @@ export default {
       mothers: [],
       filteredMothers: [],
       members: [],
-      names: [],
       types: [],
 
+      showFilter: false,
       typeFilter: '',
       memberFilter: '',
       haveFilter: false,
       nameFilter: '',
       availableFilter: false
+    }
+  },
+  computed: {
+    memberFilterName () {
+      let result = ''
+      if (this.memberFilter) {
+        // const filterId = parseInt(this.memberFilter, 10)
+        this.members.some(({ id, name }) => {
+          if (id === this.memberFilter) {
+            result = name
+            return true
+          }
+        })
+      }
+      return result
+    },
+    names () {
+      return this.filteredMothers.map(({ name }) => name).sort(compare)
     }
   },
   watch: {
@@ -220,6 +269,14 @@ export default {
         }
         return true
       })
+    },
+    clearFilter () {
+      this.typeFilter = ''
+      this.memberFilter = ''
+      this.haveFilter = false
+      this.nameFilter = ''
+      this.availableFilter = false
+      this.showFilter = false
     },
     you (owner, caps) {
       if (owner.id === this.user.id) {
