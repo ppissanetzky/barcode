6f7dc8b84e2ab92428080b63df94a0e3a59ce504
Pablo Pissanetzky <ppissanetzky@netflix.com>
2021-02-12 09:05:29 -0800

    Restart containers. Start them detached

--- containers/barcode.bareefers.org/target/start.sh
+++ containers/barcode.bareefers.org/target/start.sh
@@ -3,4 +3,4 @@ docker-compose down
 docker load < ./web-server.tgz
 docker load < ./api-server.tgz
 docker-compose build
-docker-compose up --scale api-server=2
+docker-compose up --scale api-server=2 -d
