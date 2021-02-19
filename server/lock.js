28af3c8e78e7994812dddb112db2b809f9bf088a
Pablo Pissanetzky <ppissanetzky@netflix.com>
2021-02-19 11:56:25 -0800

    Hold scheduler job locks for 5 minutes, to give them less chance of stepping on each other

--- server/lock.js
+++ server/lock.js
@@ -3,6 +3,7 @@ const path = require('path');
 const assert = require('assert');
 
 const lockfile = require('proper-lockfile');
+const ms = require('ms');
 
 const {BC_DATABASE_DIR} = require('./barcode.config');
 
@@ -16,7 +17,11 @@ function lock(name, f) {
     const lockfilePath = path.join(BC_DATABASE_DIR, `${name}.lock`);
     return lockfile.lock(BC_DATABASE_DIR, {lockfilePath})
         .then(
-            (release) => (new Promise((resolve) => resolve(f()))).finally(release),
+            (release) => new Promise(resolve => resolve(f()))
+                // Hold the lock for 5 minutes, so that quick tasks still
+                // exclude each other
+                .finally(() => setTimeout(release, ms('5 minutes')))
+            ,
             () => console.log('Failed to get lock', name)
         )
 }
